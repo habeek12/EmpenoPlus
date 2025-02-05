05d6e8101d47dd3e317b72d5046c9912dd170bb5
habeek12 <kimnayeonjeni@gmail.com>
2025-02-05 11:46:57 -0600

    FIX - Create Project

---
+++ src/main/java/com/empenoplus/EmpenoPlusApplication.java
@@ -0,0 +1,11 @@
+package com.empenoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpenoPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmpenoPlusApplication.class, args);
+    }
+}
